eabc9be399c6247c00dc0ed7f19836a2cdecf10b
Allegra Pusateri <allegrapusateri@gmail.com>
2019-10-01 14:31:49 -0500

    server updates

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '/'), function (err) {
+    res.sendFile(path.join(__dirname, 'public/index.html'), function (err) {
         if (err) {
             res.status(500).send(err)
         }

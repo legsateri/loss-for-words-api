164fa39312bdd1bde6502285bc05448c9e0514ae
Allegra Pusateri <allegrapusateri@gmail.com>
2020-01-26 20:38:24 -0600

    add auth, users, and middleware + refactor comments endpoint

--- src/comments/comments-router.js
+++ src/comments/comments-router.js
@@ -1,17 +1,13 @@
+////////////////////////////////////////////////////////////////////////////////
 const path = require('path');
 const express = require('express');
-const xss = require('xss');
+////////////////////////////////////////////////////////////////////////////////
 const CommentsService = require('./comments-service');
-
+const { requireAuth } = require('../middleware/jwt-auth');
+////////////////////////////////////////////////////////////////////////////////
 const commentsRouter = express.Router();
 const jsonParser = express.json();
-
-const serializeComment = comment => ({
-    id: comment.id,
-    prompt_response: xss(comment.prompt_response),
-    author: xss(comment.author),
-    prompt_id: comment.prompt_id
-});
+////////////////////////////////////////////////////////////////////////////////
 
 commentsRouter
     .route('/')
@@ -20,32 +16,50 @@ commentsRouter
         const knexInstance = req.app.get('db');
         CommentsService.getAllComments(knexInstance)
             .then(comments => {
-                res.json(comments.map(serializeComment));
+                res.json(comments);
             })
             .catch(next)
     })
 
-    .post(jsonParser, (req, res, next) => {
-        const { prompt_response, author, prompt_id } = req.body;
-        const newComment = { prompt_response, author, prompt_id };
+    .post(requireAuth, jsonParser, (req, res, next) => {
+        const { prompt_response, prompt_id } = req.body;
+        const newComment = { prompt_response, prompt_id };
 
-        for (const [key, value] of Object.entries(newComment)) {
-            if (value == null) {
+        for (const [key, value] of Object.entries(newComment))
+            if (value == null)
                 return res.status(400).json({
                     error: { message: `Missing ${key} in request` }
-                });
-            }
-        }
-        const knexInstance = req.app.get('db');
+                })
+
+        newComment.user_id = req.user_user_id
+
         CommentsService.insertComments(
-            knexInstance,
+            req.app.get('db'),
             newComment
         )
             .then(comment => {
                 res
                     .status(201)
                     .location(path.posix.join(req.originalUrl, `/${comment.id}`))
-                    .json(serializeComment(comment))
+                    .json(CommentsService.serializeComment(comment))
+            })
+            .catch(next)
+    })
+
+commentsRouter
+    .route('/user')
+
+    .all(requireAuth)
+
+    .get((reg, res, next) => {
+        const user_id = req.user.user_id
+
+        CommentsService.getCommentsByUser(
+            req.app.get('db'),
+            user_id
+        )
+            .then(comments => {
+                res.json(comments.map(CommentsService.serializeCommentWithUser))
             })
             .catch(next)
     })
@@ -53,34 +67,73 @@ commentsRouter
 commentsRouter
     .route('/:comment_id')
 
+    .all(requireAuth)
+
     .all((req, res, next) => {
-        const knexInstance = req.app.get('db');
-        const routeParameter = req.params.comment_id;
-        CommentsService.getById(knexInstance, routeParameter)
+        CommentsService.getById(
+            req.app.get('db'),
+            req.params.id
+        )
             .then(comment => {
                 if (!comment) {
                     return res.status(404).json({
-                        error: { message: 'Comment does not exist' }
-                    });
+                        error: { message: `Comment does not exist` }
+                    })
                 }
-
                 res.comment = comment;
                 next()
             })
-            .catch(next);
+            .catch(next)
     })
 
     .get((req, res, next) => {
-        res.json(serializeComment(res.comment));
+        res.json(res.comment);
     })
-    
+
     .delete((req, res, next) => {
         CommentsService.deleteComments(
             req.app.get('db'),
-            req.params.comment_id
+            req.params.id
         )
-            .then(numRowsAffected => {
+            .then(() => {
                 res.status(204).end();
+            })
+            .catch(next)
+    })
+
+    .patch(jsonParser, (req, res, next) => {
+        const { comment } = req.body
+        updatedComment = {comment}
+
+        CommentsService.updateComment(
+            req.app.get('db'),
+            req.params.id,
+            updatedComment
+        )
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(next)
+    })
+
+commentsRouter
+    .route('/clubs/:club_id')
+
+    .all(requireAuth)
+
+    get((req, res, next) => {
+        CommentsService.getCommentsByPrompt(
+            req.app.get('db'),
+            req.params.prompt_id,
+            req.user.user_id
+        )
+            .then(comments => {
+                if(!comments.length) {
+                    return res.status(404).json({
+                        error: {message: `No comments exist for this prompt.`}
+                    })
+                }
+                res.json(comments.map(CommentsService.serializeCommentWithUser))
             })
             .catch(next)
     })
--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -1,31 +1,123 @@
-const CommentsService = {
-    getAllComments(knex) {
-        return knex.select('*').from('comments');
-    },
+////////////////////////////////////////////////////////////////////////////////
+const xss = require('xss')
+////////////////////////////////////////////////////////////////////////////////
 
-    insertComments(knex, newComments) {
+const CommentsService = {
+    insertComments(knex, newComment) {
         return knex
-            .insert(newComments)
+            .insert(newComment)
             .into('comments')
             .returning('*')
-            .then(rows => {
-                return rows[0]
-            });
+            .then(([comment]) => comment)
     },
 
-    deleteComments(knex, id) {
+    serializeComment(comment) {
+        return {
+            id: comment.id,
+            prompt_response: xss(comment.prompt_response),
+            prompt_id: comment.prompt_id,
+            user_id: comment.user_id
+        }
+    },
+
+    serializeCommentWithPromptInfo(comment) {
+        const { prompt } = comment
+        return {
+            id: comment.id,
+            prompt_response: comment.prompt_response,
+            prompt_id: comment.prompt_id,
+            user_id: comment.user_id,
+            prompt: {
+                id: prompt.id,
+                prompt_content: prompt.prompt_content,
+                category: prompt.category,
+                author: prompt.author
+            }
+        }
+    },
+
+    getCommentsByUser(db, user_id) {
+        return db
+            .from('comments AS comment')
+            .select(
+                'comment.id',
+                'comment.prompt_id',
+                'comment.prompt_response',
+                'comment.user_id',
+                db.raw(
+                    `row_to_json(
+                            (SELECT tmp FROM (
+                                SELECT
+                                    user.user_id,
+                                    user.full_name,
+                                    user.email,
+                                    user.date_modified
+                            ) tmp)
+                        )
+                    ) AS "user"`
+                )
+            )
+            .leftJoin(
+                'users AS user',
+                'comment.user_id',
+                'user.user_id',
+                'user.full_name'
+            )
+            .where('comment.id', id)
+            .first()
+    },
+
+    getAllComments(knex) {
+        return knex
+            .select('*')
+            .from('comments')
+    },
+
+    getById(knex, id) {
+        return knex('comments')
+            .where('id', id)
+            .first()
+    },
+
+    deleteComment(knex, id) {
         return knex('comments')
             .where({ id })
             .delete()
     },
 
-    getById(knex, id) {
-        return knex
-            .from('comments')
-            .select('*')
-            .where('id', id)
-            .first()
+    updateComment(knex, id, updatedComment) {
+        return knex('comments')
+            .where({id})
+            .update(updatedComment)
+    },
+
+    getCommentsByPrompt(db, club_id, user_id) {
+        return db
+            .from('comments AS comment')
+            .select(
+                'comment.prompt_response',
+                'comment.prompt_id',
+                'comment.user_id',
+                'user.full_name'
+            )
+            .where('comment.prompt_id', prompt_id)
+            .leftJoin(
+                'users AS user',
+                'comment.user_id',
+                'user.user_id'
+            )
+    },
+
+    serializeCommentWithUser(comment) {
+        return {
+            id: comment.id,
+            prompt_response: xss(comment.prompt_response),
+            prompt_id: comment.prompt_id,
+            user_id: comment.user_id,
+            full_name: comment.full_name
+        }
     }
+
 }
 
 module.exports = CommentsService;
